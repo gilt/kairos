0d743c211f1cfd755eb6e8c15385cd1f10b443d3
Kevan Davis <kdavis@gilt.com>
2013-03-29 11:53:04 -0400

    Update examples to use the new API
    Also, made the bouncy balls example (game_engine.html) more fun.
    Try clicking.

--- examples/job_runner.js
+++ examples/job_runner.js
@@ -2,122 +2,31 @@
 var
   kairos = require('../dist/kairos'),
 
-  scheduler = new kairos.KairosScheduler({
-    times: {
-      'now': (new Date())
-    },
-    frames: [{
-      begin: {
-        starting: '60s',
-        after: 'now'
-      },
-      name: 'oneTimeJob',
-      data: {
+  collection = new kairos.KairosCollection([
+    new kairos.KairosTimeFrame('oneTimeJob')
+      .setBeginsAt('60s after now')
+      .setData({
         foo: 'bar'
-      }
-    }, {
-      interval: 5000,
-      name: 'periodicJob',
-      data: {
+      }),
+    new kairos.KairosTimeFrame('periodicJob')
+      .setTicksEvery('5 seconds')
+      .setData({
         x: 1337,
         y: 11
-      }
-    }]
+      })
+  ]).subscribe('periodicJob/ticked', function (frame) {
+    console.info('DOING SOMETHING REPEATEDLY', frame.getData().x, frame.getData().y);
+    frame.getData().x += 1;
+    frame.getData().y = frame.getData().x % 13;
+  }).subscribe('oneTimeJob/began', function (frame) {
+    console.info('DOING A ONE TIME JOB: foo=', frame.getData().foo);
+  }).subscribe('newJob/began', function () {
+    console.info('DOING SOMETHING ELSE');
   });
 
-scheduler.subscribe('periodicJob/ticked', function (duration, data) {
-  console.info('DOING SOMETHING REPEATEDLY', data.x, data.y);
-  data.x += 1;
-  data.y = data.x % 13;
-});
+collection.pushTimeFrame(
+  new kairos.KairosTimeFrame('newJob')
+    .setBeginsAt('PT15S from now')
+);
 
-scheduler.subscribe('oneTimeJob/started', function (duration, data) {
-  console.info('DOING A ONE TIME JOB: foo=', data.foo);
-});
-
-scheduler.subscribe('newJob/started', function () {
-  console.info('DOING SOMETHING ELSE');
-});
-
-scheduler.addFrame({
-  name: 'newJob'
-});
-
-
-
-
-
-var
-  times = {
-    breakfast: 123498298234,
-    lunch: 123123123123,
-    dinner: 234234234123
-  },
-  sampleFrame = new KairosFrame(),
-  scheduler = new KairosScheduler([
-    sampleFrame,
-    new KairosFrame('foo'),
-
-    new KairosFrame('morning', { // WINNER!!!
-      beginsAt: times.breakfast,
-      endsAt: '1 hour before lunch',
-      ticksEvery: '1s',
-      relativeTo: 'lunch'
-    }).withTimes(times), // each time is immutable, builds up a dictionary
-
-    new KairosFrame('afternoon')
-
-      // jQuery style:
-      .beginsAt(times.lunch)
-      .beginsAt()
-      .endsAt('dinner', times)
-      .endsAt()
-      .ticksEvery('1s')
-      .ticksEvery()
-      .relativeTo(times.lunch)
-      .relativeTo()
-
-      // get/set style:
-      .setBeginsAt(asdf)
-      .getBeginsAt()
-      .setEndsAt(asdf)
-      .getEndsAt()
-      .setTicksEvery(asdf)
-      .getTicksEvery()
-      .setRelativeTo(asdf)
-      .getRelativeTo()
-
-      // natural language style:
-      .beginAt(asdf)
-      .beginsAt()
-      .endAt(asdf)
-      .endsAt()
-      .tickEvery(asdf)
-      .ticksEvery()
-      .relativeTo(asdf)
-      .isRelativeTo() // not sure about this
-
-
-
-
-      .subscribe('started', fn)
-      .subscribe('ended', fn)
-      .subscribe('ticked', fn)
-
-      .start()
-      .pause()
-      .resume(),
-
-    new KairosFrame('evening')
-      .beginsAt(horo('dinner', times))
-      .endsAt(horo('midnight ET after dinner')(times))
-      .ticksEvery(horo('every second before lunch and every other second after lunch')),
-
-    new KairosFrame('tea')
-      .beginsAt(horo('tres horas antes de lunch', 'es'))
-      .endsAt(horo('una hora antes de inicio', 'es'))
-      .withTimes(times)
-  ]).withTimes(times);
-
-sampleFrame.beginsAt(123123123123);
-scheduler.findFrame('foo').whenDoesItEnd();
+collection.start();
